da674311252212a1ebdc05f77e8b1c128d12434d
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2023-01-05 14:26:05 +0100

    all errors gone

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-setURL('http://gruppe-join-421.developerakademie.net/smallest_backend_ever.zip');
+setURL(`http://gruppe-join-421.developerakademie.net/smallest_backend_ever`);
 
 async function init() {
-    await downloadFromServer();
-    users = JSON.parse(backend.getItem('users')) || [];
+    // await downloadFromServer();
+    // users = JSON.parse(backend.getItem('users')) || [];
     awaitHTML();
 }
-
-
