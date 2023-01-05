c8334ef0010b629cd04d322a688f18b4c4365eac
Pesendorfer90 <pesendorfer.gerald@hotmail.com>
2023-01-05 13:21:48 +0100

    folder structure

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ setURL('http://developerakademie.com/smallest_backend_ever');
 async function init() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
+    awaitHTML();
 }
+
+
