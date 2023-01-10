93e9fa7a79faca10f386301c40c8040322e67863
rOBI <robertjamesde123@gmail.com>
2023-01-10 23:21:52 +0100

    board update

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,44 +1,56 @@
 
-let myData = [
-    {
-        "category": "Sales",
-        "title": "Call Potential Clients",
-        "description": "Make the Product Presentation to prospective Buyers",
-        "assigned_to": ['john'],
-        "due_date": "20.02.2150",
-        "prio": "Urgent",
-        "subtasks": 1
-    },
-    {
-        "category": "Sales",
-        "title": "Call Potential Clients",
-        "description": "Make the Product Presentation to prospective Buyers",
-        "assigned_to": ['john'],
-        "due_date": "20.02.2150",
-        "prio": "Urgent",
-        "subtasks": 1
-    }, {
-        "category": "Sales",
-        "title": "Call Potential Clients",
-        "description": "Make the Product Presentation to prospective Buyers",
-        "assigned_to": ['john'],
-        "due_date": "20.02.2150",
-        "prio": "Urgent",
-        "subtasks": 1
-    }, {
-        "category": "Sales",
-        "title": "Call Potential Clients",
-        "description": "Make the Product Presentation to prospective Buyers",
-        "assigned_to": ['John', 'Melissa'],
-        "due_date": "20.02.2150",
-        "prio": "Urgent",
-        "subtasks": 1
-    },
-];
+// let myData = [
+//     {
+//         "category": "Sales",
+//         "title": "Call Potential Clients",
+//         "description": "Make the Product Presentation to prospective Buyers",
+//         "assigned_to": ['john'],
+//         "due_date": "20.02.2150",
+//         "prio": "Urgent",
+//         "subtasks": 1
+//     },
+//     {
+//         "category": "Sales",
+//         "title": "Accounting Invoices",
+//         "description": "Make the Product Presentation to prospective Buyers",
+//         "assigned_to": ['john'],
+//         "due_date": "20.02.2150",
+//         "prio": "Urgent",
+//         "subtasks": 1
+//     }, {
+//         "category": "Sales",
+//         "title": "Social Media Strategy",
+//         "description": "Make the Product Presentation to prospective Buyers",
+//         "assigned_to": ['john'],
+//         "due_date": "20.02.2150",
+//         "prio": "Urgent",
+//         "subtasks": 1
+//     }, {
+//         "category": "Sales",
+//         "title": "Call Potential Clients",
+//         "description": "Make the Product Presentation to prospective Buyers",
+//         "assigned_to": ['John', 'Melissa'],
+//         "due_date": "20.02.2150",
+//         "prio": "Urgent",
+//         "subtasks": 1
+//     },
+// ];
 
+// let myData;
+
+let filteredData = [];
 
 let backgroundColors = ["8AA4FF", "FF0000", "2AD300", "FF7A00", "E200BE", "0038FF"];
 let progress = ['To Do', 'In Progress', 'Awaiting Feedback', 'Done'];
+
+
+async function loadDataBase() {
+    let resp = await fetch('.//database.json');
+    let myData = await resp.json();
+    renderToDoDetails(myData);
+    
+    filteredData.push(myData);
+}
 
 
 function renderProgressSection() {
@@ -50,6 +62,7 @@ function renderProgressSection() {
 
     }
 }
+
 
 function renderProgressSectionHtml(oneSection) {
     return         /*html*/ `
@@ -65,17 +78,19 @@ function renderProgressSectionHtml(oneSection) {
 `;
 }
 
-function renderToDoDetails() {
+
+function renderToDoDetails(myData) {
     for (let i = 0; i < myData.length; i++) {
-        oneCard = myData[i];
+        let oneCard = myData[i];
         let title = oneCard['title'];
         let description = oneCard['description'];
         let category = oneCard['category'];
         let prio = oneCard['prio'];
-
         let card = document.getElementById('cards');
+        
         card.innerHTML += renderToDoDetailsHtml(title, description, category, i);
-        renderCoWorkers(oneCard, i);
+        
+        renderCoWorkers(myData,i);
     }
 }
 
@@ -109,10 +124,31 @@ function renderToDoDetailsHtml(title, description, category, i) {
 `;
 }
 
-function renderCoWorkers(oneCard, i) {
-    let myCard = oneCard['assigned_to'];
+
+function renderCoWorkers(myData ,i) {
+    let myCard = myData[i]['assigned_to'];
     for (let c = 0; c < myCard.length; c++) {
         const element = myCard[c].charAt(0);
         document.getElementById(`initials${i}`).innerHTML += `<div class="initials" >${element}</div>`;
+    }
+}
+
+
+function filterCards() {
+    let search = document.getElementById('myInput').value;
+    search = search.toLowerCase();
+   
+    let card = document.getElementById('cards');
+    card.innerHTML = '';
+    
+    for (let i = 0; i < filteredData.length; i++) {
+        const filteredCards = filteredData[i];
+        for (let s = 0; s < filteredCards.length; s++) {
+            const myTitle = filteredCards[s]['title'];
+            if(myTitle.toLowerCase().includes(search)){
+                renderToDoDetails(filteredCards);
+                console.log(myTitle);
+            }
+        }
     }
 }
