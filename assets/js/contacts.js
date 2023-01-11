7b8d4e1e9bde901c9062297f301164e1741c1a4a
AlinaWetter <alinawetter00@gmail.com>
2023-01-11 15:55:52 +0100

    contacts json

--- assets/js/contacts.js
+++ assets/js/contacts.js
@@ -1,4 +1,5 @@
 let contacts = [];
+let letters = [];
 
 function submitContact() {
     let name = document.getElementById('name');
@@ -25,6 +26,8 @@ function submitContact() {
         console.log(contacts);
     }
 
+    closeNewContact();
+
 }
 
 function WordCount(str) {
@@ -32,6 +35,59 @@ function WordCount(str) {
     return arr.filter(word => word !== '').length;
 }
 
+function openCreateContact() {
+    document.getElementById('overlay').classList.remove('d-none')
+    fadeIn();
+    slideIn();
+}
 
+function closeNewContact() {
+    slideOut();
+    fadeOut();
+    setTimeout(function () {
+        document.getElementById('overlay').classList.add('d-none')
+    }, 400)
+    createLetters();
+}
 
+function fadeIn() {
+    document.getElementById('overlay').classList.remove('fade-out');
+    document.getElementById('overlay').classList.add('fade-in');
+}
 
+function slideIn() {
+    document.getElementById('newContactContainer').classList.remove('slide-out');
+    document.getElementById('newContactContainer').classList.add('slide-in');
+}
+
+function fadeOut() {
+    document.getElementById('overlay').classList.remove('fade-in');
+    document.getElementById('overlay').classList.add('fade-out');
+}
+
+function slideOut() {
+    document.getElementById('newContactContainer').classList.remove('slide-in');
+    document.getElementById('newContactContainer').classList.add('slide-out');
+}
+
+function createLetters() {
+    document.getElementById('contacts').innerHTML = '';
+
+    for (j = 0; j < contacts.length; j++) {
+        let str = contacts[j]['firstname'];
+        if (!letters.includes(str.charAt(0))) {
+            letters.push(str.charAt(0));
+        }
+    }
+
+    for(i = 0; i < letters.length; i++) {
+        document.getElementById('contacts').innerHTML += createLetterHtml(i);
+    }
+}
+
+function createLetterHtml(i) {
+    return /*html*/`
+        <div class="letter" onclick="searchLetter(${i})">${letters[i]}</div>
+        <div></div>
+    `;
+}
