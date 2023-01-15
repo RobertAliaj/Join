343bcdacb485e5d120d427ca542958ac7bab858f
rOBI <robertjamesde123@gmail.com>
2023-01-15 16:28:29 +0100

    drag and drop finish

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,62 +1,81 @@
 
-let progressJson = [
+let tasks = [
     {
-        'progress': 'To Do'
+        "id": 0,
+        "category": "Sales",
+        "title": "call",
+        "description": "Make the Product Presentation to prospective Buyers",
+        "assigned_to": ["Michael"],
+        "due_date": "20.02.2150",
+        "prio": "Urgent",
+        "subtasks": 1,
+        "progress": "TODO"
     },
     {
-        'progress': 'In Progress'
-
+        "id" : 1,
+        "category": "Sales",
+        "title": "accounting Invoices",
+        "description": "Make the Product Presentation to prospective Buyers",
+        "assigned_to": ["John", "Andy", "Mary"],
+        "due_date": "20.02.2150",
+        "prio": "Urgent",
+        "subtasks": 1,
+        "progress": "TODO"
     },
     {
-        'progress': 'Awaiting Feedback'
-    },
-    {
-        'progress': 'Done'
-    }
-]
+        "id": 2,
+        "category": "Marketing",
+        "title": "Social media strategy",
+        "description": "Develop an ad campaign for brand positioning",
+        "assigned_to": ["Ina", "Dita", "Mary", "Anna"],
+        "due_date": "2023-03-02",
+        "prio": "low",
+        "subtasks": "Design an ad, Calculate the costs, Created accounts for all common social media platforms",
+        "progress": "TODO"
+      },
+      {
+        "id": 3,
+        "category": "Media",
+        "title": "Video cut",
+        "description": "Edit the new company video",
+        "assigned_to": ["Jani", "Rrezi", "Mary", "Anna", "Gary", "Mary", "Anna", "Gary"],
+        "due_date": "2023-03-02",
+        "prio": "medium",
+        "subtasks": "",
+        "progress": "TODO"
+      }
+];
 
-let myData = [];
 
-let currentDragElement;
+let progress =['TODO','inProgress','feedback','done'];
 
-async function renderContent() {
-    await loadMyData();
-    renderProgressSection();
-    renderCards();
-}
-
-
-async function loadMyData() {
-    let resp = await fetch('.//database.json');
-    myData = await resp.json();
-}
-
-
-function renderProgressSection() {
-    let mySection = document.getElementById('toDoSection');
-    for (let i = 0; i < progressJson.length; i++) {
-        const currentSection = progressJson[i]['progress'];
-        mySection.innerHTML += renderProgressHtml(currentSection, i);
-    }
-}
+let currentDraggedElement;
 
 
 function renderCards() {
-    let tasks = document.getElementById(`task`);
-    tasks.innerHTML = '';
-
-    for (let l = 0; l < myData.length; l++) {
-        const currentCard = myData[l];
-        tasks.innerHTML += renderCardsHtml(currentCard, l);
-        renderCoWorkes(l);
+    clearSections();
+    for (let i = 0; i < tasks.length; i++) {
+        let element = tasks[i];
+        let section = tasks[i]['progress'];
+        if (section == "TODO" || section == "inProgress" || section == "feedback" || section == "done")
+            document.getElementById(section).innerHTML += renderCardsHTML(element, i);
+            renderCoWorkes(i);
     }
+}
+
+
+function clearSections(){
+    document.getElementById('TODO').innerHTML = '';
+    document.getElementById('inProgress').innerHTML = '';
+    document.getElementById('feedback').innerHTML = '';
+    document.getElementById('done').innerHTML = '';
 }
 
 
 //render the first three CoWorkers to have the Task
-function renderCoWorkes(l) {
-    let initialsDiv = document.getElementById(`initials${l}`);
-    let initialsArray = myData[l]['assigned_to'];
+function renderCoWorkes(i) {
+    let initialsDiv = document.getElementById(`initials${i}`);
+    let initialsArray = tasks[i]['assigned_to'];
     //das 0 wird praktisch nicht durch die Schleife erhöht sondern durch divscreated++; 
     let divsCreated = 0;
     for (let s = 0; s < initialsArray.length; s++) {
@@ -83,44 +102,156 @@ function displayRemainingWorkers(initialsArray, initialsDiv) {
 }
 
 
-function filterCards() {
+function searchTask() {
     let search = document.getElementById('searchInput').value;
     search = search.toLowerCase().trim();
-    let task = document.getElementById('task');
-    task.innerHTML = '';
 
-    for (let l = 0; l < myData.length; l++) {
-        const currentCard = myData[l];
-        if (myData[l]['title'].toLowerCase().includes(search)) {
-            task.innerHTML += renderCardsHtml(currentCard, l);
-            renderCoWorkes(l);
+    for (let i = 0; i < progress.length; i++) {
+        const status = progress[i];
+        document.getElementById(status).innerHTML = '';
+    }
+
+    document.getElementById('TODO').innerHTML = '';
+
+    for (let i = 0; i < tasks.length; i++) {
+        const element = tasks[i];
+        if (tasks[i]['title'].toLowerCase().includes(search)) {
+            document.getElementById('TODO').innerHTML += renderCardsHTML(element, i);
+            renderCoWorkes(i);
         }
     }
 }
+
 
 function allowDrop(ev) {
     ev.preventDefault();
 }
 
 
-
-function dragOver(ev) {
-    ev.preventDefault();
-}
-
-
 function startDragging(id) {
-    currentDragElement = id;
+    currentDraggedElement = id;
+}
+
+function moveTo(progress){
+    tasks[currentDraggedElement]['progress'] = progress;
+    console.log(tasks[currentDraggedElement]['progress']);
+    renderCards(); 
 }
 
 
-function moveTo(currentSection, i) {
-    document.getElementById('toDoSection').innerHTML = '';
-    progressJson[i]['progress'] = currentSection;
-    renderProgressSection();
-    renderCards();
-    console.log(progressJson[i]['progress']);
+function renderCardsHTML(element, i) {
+    return`
+    <div class="to-do-cards" draggable="true" ondragstart="startDragging(${element['id']})">
+         <div class="category" id="category">
+             ${element['category']}
+         </div>
+   
+         <div class="title-descr">
+             <span class="to-do-title" id="toDoTitle">
+                 ${element['title']}
+             </span>
+             <span>
+                 ${element['description']}
+             </span>
+         </div> 
+         <div class="progress-div">
+             <div class="progress-bar"></div>
+             <div><span class="progress-info"> <b>1</b>/ <b>2</b> Done</span></div>
+         </div>
+
+         <div id="initials${i}" class="initials-div">
+         </div>
+     </div>
+    `; 
 }
+
+
+function renderCoWorkersHtml(myInitial, s) {
+    return `
+    <div class="initials" id="inits${s}">
+     ${myInitial}
+    </div>
+    `;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// async function loadMyData() {
+//     let resp = await fetch('.//database.json');
+//     myData = await resp.json();
+// }
+
+
+
+
+
+
+// function filterCards() {
+//     let search = document.getElementById('searchInput').value;
+//     search = search.toLowerCase().trim();
+//     let task = document.getElementById('task');
+//     task.innerHTML = '';
+
+//     for (let l = 0; l < myData.length; l++) {
+//         const currentCard = myData[l];
+//         if (myData[l]['title'].toLowerCase().includes(search)) {
+//             task.innerHTML += renderCardsHtml(currentCard, l);
+//             renderCoWorkes(l);
+//         }
+//     }
+// }
+
+
+
+
 
 
 function displayOverlay(){
@@ -132,53 +263,4 @@ function displayOverlay(){
 function removeOverlay(){
     document.getElementById('overlayBg').classList.add('d-none');
     document.getElementById('popUp').classList.add('d-none')
-}
-
-
-function renderProgressHtml(currentSection, i) {
-    return `
-    <div class="task-divs-parent" id="${currentSection}" ondrop="moveTo('${currentSection}', ${i})" ondragover="allowDrop(event)">
-    <div class="task-divs-child">
-       <div>${currentSection}</div>
-     <img src="./assets/img/add_button.png" class="add-img" onclick="displayOverlay()">
-    </div>
-
-        <div id="task" class="cards-parent">
-        </div>`;
-}
-
-
-function renderCardsHtml(currentCard, l) {
-    return `
-    <div class="to-do-cards" id="myCard${l}" draggable="true"  ondragstart="startDragging(${l})">
-        <div class="category" id="category">
-            ${currentCard['category']}
-        </div>
-   
-        <div class="title-descr">
-            <span class="to-do-title" id="toDoTitle">
-                ${currentCard['title']}
-            </span>
-            <span>
-                ${currentCard['description']}
-            </span>
-        </div> 
-        <div class="progress-div">
-            <div class="progress-bar"></div>
-            <div><span class="progress-info"> <b>1</b>/ <b>2</b> Done</span></div>
-        </div>
-
-        <div id="initials${l}" class="initials-div">
-        </div>
-    </div>
-`;
-}
-
-
-function renderCoWorkersHtml(myInitial, s) {
-    return `
-    <div class="initials" id="inits${s}">
-     ${myInitial}
-    </div>
-    `;
 }
