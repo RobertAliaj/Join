95425097d3b43851f1000d74c2de7aec507cf850
rOBI <robertjamesde123@gmail.com>
2023-01-17 18:48:29 +0100

    commit

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 setURL(`https://gruppe-join-421.developerakademie.net/smallest_backend_ever`);
 
-<<<<<<< HEAD
-async function init() {
-    await downloadFromServer();
-}
-=======
 // async function init() {
-//     await downloadFromServer();
+// //     await downloadFromServer();
 // }
->>>>>>> 21120911eb2a5222f7078fd58b94c4f27f6d6181
