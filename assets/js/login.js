a847decd370b4885ad75bfcb0b5278463c3c4b36
AlinaWetter <alinawetter00@gmail.com>
2023-01-31 13:52:05 +0100

    login

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,34 @@
 function animate() {
-    const animation = gsap.timeline()
-    .set("#imgDiv", {
-        xPercent: -50,
-        yPercent: -50,
-        x: innerWidth / 2,
-        y: innerHeight / 2,
-        scale: 0.9
-    })
-    .to("#imgDiv", {
-        xPercent: 0,
-        yPercent: 0,
-        x: 0,
-        y: 0,
-        scale: 0.3,
-        ease: "power1.out",
-        duration: 1
-    }, "+=.5");
-}
+    setTimeout(function() {
+      const animation = gsap.timeline()
+        .set("#imgDiv", {
+          // xPercent: -50,
+          // yPercent: -50,
+          x: 0,
+          y: 0,
+          // x: innerWidth / 2,
+          // y: innerHeight / 2,
+          scale: 0.9
+        }, 0)
+        .set("#contentDiv", {
+          opacity: 0
+        }, 0)
+        .to("#imgDiv", {
+          xPercent: 0,
+          yPercent: 0,
+          // x: 0,
+          // y: 0,
+          x: - innerWidth / 2,
+          y: - innerHeight / 2,
+          scale: 0.3,
+          ease: "power1.out",
+          duration: 1
+        }, 0)
+        
+        .to("#contentDiv", {
+          opacity: 1,
+          duration: 1
+        }, 0);
+    }, 1000);
+  }
+  
