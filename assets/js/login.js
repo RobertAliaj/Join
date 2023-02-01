61dc03afcb98995641ef970453e0c21e541b8dc3
AlinaWetter <alinawetter00@gmail.com>
2023-02-01 22:01:55 +0100

    remember_me_function

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,6 +4,7 @@ async function init() {
   await downloadFromServer();
   users = jsonFromServer['users'];
   openLogIn();
+  lsRememberMe();
 }
 
 function animate() {
@@ -43,14 +44,14 @@ function saveGreetingName(name) {
 
 function openSignUp() {
   document.getElementById('signUp').style.display = 'none';
-  loginContainer = document.getElementById('loginContainer')
+  let loginContainer = document.getElementById('loginContainer')
   loginContainer.innerHTML = '';
   loginContainer.innerHTML += SignUpContainerHtml();
 }
 
 function openLogIn() {
   document.getElementById('signUp').style.display = 'flex';
-  loginContainer = document.getElementById('loginContainer')
+  let loginContainer = document.getElementById('loginContainer')
   loginContainer.innerHTML = '';
   loginContainer.innerHTML += LoginContainerHtml();
 }
@@ -73,54 +74,97 @@ async function pushUser(user) {
   await saveJSONToServer();
 }
 
+function logIn() {
+  let email = document.getElementById('email');
+  let password = document.getElementById('password');
+  let user = users.find(u => u.email == email.value && u.password == password.value);
+  if (user) {
+    // alert.classList.add('d-none');
+    
+    saveGreetingName(user['name'])
+    location.href = 'index.html';
+
+  } else {
+    email.value = '';
+    password.value = '';
+    let alert = document.getElementById('alert');
+    alert.classList.remove('d-none');
+  }
+}
+
+
+
+function lsRememberMe() {
+  const rmCheck = document.getElementById('rememberMe'),
+    emailInput = document.getElementById('email');
+
+  if (localStorage.checkbox && localStorage.checkbox !== "") {
+    rmCheck.setAttribute("checked", "checked");
+    emailInput.value = localStorage.username;
+  } else {
+    rmCheck.removeAttribute("checked");
+    // emailInput.value = "";
+  }
+
+  if (rmCheck.checked) {
+    localStorage.username = emailInput.value;
+    localStorage.checkbox = "checked";
+  } else {
+    localStorage.username = "";
+    localStorage.checkbox = "";
+  }
+}
+
 function LoginContainerHtml() {
   return `
-      <span>Log in</span>
+      <span class="login-span">Log in</span>
       <div class="horizontal-blue-line"></div>
       <form onsubmit="return false">
         <div>
-          <input type="email" placeholder="Email" id="email">
+          <input autocomplete="username" type="email" placeholder="Email" id="email">
           <img src="assets/img/mail.png" alt="">
         </div>
         <div>
-          <input type="password" placeholder="Password" id="password">
+          <input autocomplete="current-password" type="password" placeholder="Password" id="password">
           <img src="assets/img/schloss.png" alt="">
         </div>
-     
-    <div class="remember-me">
-    <div class="select-box"></div>
-      <span>Remember me</span>
-      <a>Forgot my password</a>
-    </div>
-    <div class="buttons">
-      <button type="submit" onclick="login()" class="login-button"> Log in</button>
-      <button type="submit" onclick="location.href = 'index.html'; saveGreetingName('Guest')"class="guest-login-button">Guest Log in</button>
-    </div> 
-    </form>
+        <div id="alert" class="alert d-none">
+          <span> You've got the wrong email or password </span>
+        </div>
+        <div class="remember-me">
+          <input type="checkbox" id="rememberMe" onclick="lsRememberMe()" value="lsRememberMe" class="select-box">
+          <span>Remember me</span>
+          <a>Forgot my password</a>
+        </div>
+        <div class="buttons">
+          <button type="submit" onclick="logIn()" class="login-button"> Log in</button>
+          <button type="submit" onclick="location.href = 'index.html'; saveGreetingName('Guest')" class="guest-login-button">Guest Log in</button>
+        </div> 
+      </form>
   `;
 }
 
 function SignUpContainerHtml() {
   return `
-    <img onclick="openLogIn()" class="arrow" src="assets/img/left-arrow.png">
-    <span class="sign-up-span" >Sign up</span>
-      <div class="horizontal-blue-line"></div>
-      <form onsubmit="return false">
-        <div>
-          <input placeholder="Name" id="signUpName" required>
-          <img src="assets/img/user.png" alt="">
-        </div>
-        <div>
-          <input type="email" placeholder="Email" id="signUpEmail" required>
-          <img src="assets/img/mail.png" alt="">
-        </div>
-        <div>
-          <input type="password" placeholder="Password" id="signUpPassword" required>
-          <img src="assets/img/schloss.png" alt="">
-        </div>
-     <div class="buttons">
-        <button type="submit" onsubmit="signUp(); openLogIn()" class="login-button"> Sign Up</button>
-     </div>
-     </form>
+      <img onclick="openLogIn()" class="arrow" src="assets/img/left-arrow.png">
+      <span class="sign-up-span" >Sign up</span>
+        <div class="horizontal-blue-line"></div>
+        <form onsubmit="return false">
+          <div>
+            <input placeholder="Name" id="signUpName" required>
+            <img src="assets/img/user.png" alt="">
+          </div>
+          <div>
+            <input type="email" placeholder="Email" id="signUpEmail" required>
+            <img src="assets/img/mail.png" alt="">
+          </div>
+          <div>
+            <input type="password" placeholder="Password" id="signUpPassword" required>
+            <img src="assets/img/schloss.png" alt="">
+          </div>
+          <div class="buttons">
+            <button type="submit" onclick="signUp(); openLogIn()" class="login-button"> Sign Up</button>
+          </div>
+      </form>
   `;
 }
