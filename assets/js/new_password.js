4b9233d9a3e44b3c83eda930030ba88a9106034f
rOBI <robertaliaj97@gmail.com>
2023-04-18 18:20:04 +0200

    update

--- assets/js/new_password.js
+++ assets/js/new_password.js
@@ -20,13 +20,13 @@ async function createNewPassword() {
     let password2 = document.getElementById('newPassword2').value;
 
     if (password1 === password2) {
-        for(let i = 0; i < users.length; i++) {
-          if(users[i]['email'] === localStorage.email) {
-            console.log(i)
-            users[i]['password'] = password1;
-            await saveJSONToServer();
-            correctPassword();
-          }
+        for (let i = 0; i < users.length; i++) {
+            if (users[i]['email'] === localStorage.email) {
+                console.log(i)
+                users[i]['password'] = password1;
+                await saveJSONToServer();
+                correctPassword();
+            }
         }
     } else {
         wrongPassword();
@@ -37,7 +37,7 @@ function correctPassword() {
     document.getElementById('loginContainer').innerHTML += `
         <div id="blueAlert" class="blue-alert">Dein Passwort wurde erfolgreich geändert. In ein paar Sekunden wirst du zur Login Seite weitergeleitet!</div>
     `;
-    setTimeout(function() {
+    setTimeout(function () {
         location.href = 'index.html';
         document.getElementById('blueAlert').classList.add('d-none')
     }, 3000)
@@ -47,8 +47,8 @@ function wrongPassword() {
     document.getElementById('loginContainer').innerHTML += `
     <div id="redAlert" class="red-alert">Deine Passwörter stimmen nicht überein. Bitte versuche es erneut!</div>
 `;
-setTimeout(function() {
-    document.getElementById('redAlert').classList.add('d-none')
-}, 3000)
+    setTimeout(function () {
+        document.getElementById('redAlert').classList.add('d-none')
+    }, 3000)
 
 }
