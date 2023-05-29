4991df4d05ea214308a71706a14f99f10a860007
Dominik Niemietz <d.niemietz97@gmx.de>
2023-05-29 17:32:31 +0200

    bug css addTaskPopUp

--- assets/js/add_task.js
+++ assets/js/add_task.js
@@ -295,14 +295,14 @@ function priority(clicked, notClicked, alsoNotClicked, img) {
       clicked
     ).style = `background-color: rgb(236, 85, 32);`;
     changeColor(img);
-    prio = "high";
+    prio = "urgent";
   }
   if (clicked == "prioMedium") {
     document.getElementById(
       clicked
     ).style = `background-color: rgb(243, 173, 50);`;
     changeColor(img);
-    prio = "mid";
+    prio = "medium";
   }
   if (clicked == "prioLow") {
     document.getElementById(
