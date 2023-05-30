eacf427d95383220ddd6932f03967a5894453f96
AlinaWetter <alinawetter00@gmail.com>
2023-05-30 14:55:53 +0200

    jsdocumentation

--- assets/js/new_password.js
+++ assets/js/new_password.js
@@ -1,11 +1,17 @@
 users = [];
 
+/**
+ * This function initilizes the password page
+ */
 async function initPassword() {
   init();
   users = jsonFromServer["users"];
   getParameter();
 }
 
+/**
+ * This function gets the parameter of the url in the email
+ */
 function getParameter() {
   var urlParams = new URLSearchParams(window.location.search);
   var email = urlParams.get("email");
@@ -13,6 +19,9 @@ function getParameter() {
   localStorage.setItem("email", decodedEmail);
 }
 
+/** 
+ * This function gets the new password out of the inputs
+ */
 async function createNewPassword() {
   let password1 = document.getElementById("newPassword1").value;
   let password2 = document.getElementById("newPassword2").value;
@@ -30,6 +39,9 @@ async function createNewPassword() {
   }
 }
 
+/**
+ * This function redirects to the login site if the new password was similar in both inputs
+ */
 function correctPassword() {
   document.getElementById("loginContainer").innerHTML += `
         <div id="blueAlert" class="blue-alert">Dein Passwort wurde erfolgreich geändert. In ein paar Sekunden wirst du zur Login Seite weitergeleitet!</div>
@@ -40,6 +52,9 @@ function correctPassword() {
   }, 3000);
 }
 
+/**
+ * This function says that the new password was not similar in both inputs.
+ */
 function wrongPassword() {
   document.getElementById("loginContainer").innerHTML += `
     <div id="redAlert" class="red-alert">Deine Passwörter stimmen nicht überein. Bitte versuche es erneut!</div>
